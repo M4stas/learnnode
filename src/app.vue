4d95af05bb86c5ce10a461459e2fb03715166ff1
Mats Mikkor <matsmikkor@pm.me>
2026-01-21 13:56:22 +0200

    Add item lsit example

--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,35 @@
 <script setup>
 import { ref } from 'vue';
+let items = ref(['Sai', 'Piim', 'Viin', 'Õlu']);
+let newItem = ref('');
 
-let message = ref('Hello Vue!');
-
+function add(){
+    if(newItem.value.trim() !== '') {
+        items.value.push(newItem.value.trim());
+    }
+    newItem.value = '';
+}
 </script>
 
 <template>
     <div class="container">
-        <h1>{{ message.split('').reverse().join('') }}</h1>
-        <button class="button is primary" @click="message = 'Hello aaa'">click me</button>
-        <input class="input" v-model="message">
+        <div class="field has-addons mt-2">
+            <div class="control is-expanded">
+                <input class="input" type="text" v-model="newItem" @keypress.enter="add">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="add">
+                    Search
+                </button>
+            </div>
+        </div>
+
+        <div class="content">
+            <h1>All items</h1>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
     </div>
     
 
