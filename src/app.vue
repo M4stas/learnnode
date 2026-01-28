b3ab0d41ce43cf69c312132e29ecb3376f0b90bc
Mats Mikkor <matsmikkor@pm.me>
2026-01-28 14:08:40 +0200

    Add modal example

--- src/app.vue
+++ src/app.vue
@@ -1,51 +1,19 @@
 <script setup>
-import { computed, ref } from 'vue';
-import ItemList from './ItemList.vue';
-let i = 1;
-let items = ref([
-    {id: i++, text: 'Sai',isDone: false},    
-    {id: i++, text: 'Piim',isDone: false},      
-    {id: i++, text: 'Viin',isDone: true},
-    {id: i++, text: 'Õlu',isDone: false},
-]);
-let newItem = ref('');
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
 
-let doneItems = computed(() => items.value.filter(i => i.isDone));
-let todoItems = computed(() => items.value.filter(i => !i.isDone));
+let modal1Active = ref(false);
 
-function add(){
-    if(newItem.value.trim() !== '') {
-        items.value.push({id: i++, text: newItem.value.trim(), isDone: false });
-    }
-    newItem.value = '';
-}
+
+
 </script>
 
 <template>
     <div class="container">
-        <div class="field has-addons mt-2">
-            <div class="control is-expanded">
-                <input class="input" type="text" v-model="newItem" @keypress.enter="add">
-            </div>
-            <div class="control">
-                <button class="button is-info" @click="add">
-                    Search
-                </button>
-            </div>
-        </div>
+        <button class="button is-primary" @click="modal1Active=true">Open modal</button>
 
-        <div class="content">
-            <ItemList :items="items" title="allItems"></ItemList>
-            <ItemList :items="doneItems" title="doneItems"></ItemList>
-            <ItemList :items="todoItems" title="toDoItems"></ItemList>
-
-        </div>  
-        
+        <Modal :active="modal1Active" @close="modal1Active=false"></Modal>
     </div>
-    
-
 </template>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/pages/ToDo.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { computed, ref } from 'vue';
+import ItemList from './components/ItemList.vue';
+let i = 1;
+let items = ref([
+    { id: i++, text: 'Sai', isDone: false },
+    { id: i++, text: 'Piim', isDone: false },
+    { id: i++, text: 'Viin', isDone: true },
+    { id: i++, text: 'Õlu', isDone: false },
+]);
+let newItem = ref('');
+
+let doneItems = computed(() => items.value.filter(i => i.isDone));
+let todoItems = computed(() => items.value.filter(i => !i.isDone));
+
+function add() {
+    if (newItem.value.trim() !== '') {
+        items.value.push({ id: i++, text: newItem.value.trim(), isDone: false });
+    }
+    newItem.value = '';
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="field has-addons mt-2">
+            <div class="control is-expanded">
+                <input class="input" type="text" v-model="newItem" @keypress.enter="add">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="add">
+                    Search
+                </button>
+            </div>
+        </div>
+
+        <div class="content">
+            <ItemList :items="items" title="All items"></ItemList>
+            <ItemList :items="doneItems" title="Done items"></ItemList>
+            <ItemList :items="todoItems" title="Todo items"></ItemList>
+
+        </div>
+
+    </div>
+
+
+</template>
+
+<style></style>
