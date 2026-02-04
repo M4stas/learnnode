aac660712999dd602d90b9b6e1c3acfcac97e671
Mats Mikkor <matsmikkor@pm.me>
2026-02-04 13:00:20 +0200

    Finish modal example

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { ref } from 'vue';
 import Modal from './components/Modal.vue';
+import ImgModal from './components/ImgModal.vue';
 
 let modal1Active = ref(false);
+let modal2Active = ref(false);
 
 
 
@@ -11,8 +13,13 @@ let modal1Active = ref(false);
 <template>
     <div class="container">
         <button class="button is-primary" @click="modal1Active=true">Open modal</button>
+        <button class="button is-link" @click="modal2Active=true">Open modal 2</button>
 
-        <Modal :active="modal1Active" @close="modal1Active=false"></Modal>
+        <Modal :active="modal1Active" @close="modal1Active=false">
+            <div class="box">I'm in a box.</div>
+        </Modal>
+
+        <ImgModal :active="modal2Active" @close="modal2Active=false" url="https://picsum.photos/1280/960?r=2"></ImgModal>
     </div>
 </template>
 
