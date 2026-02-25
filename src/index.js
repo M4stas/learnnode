aad704b1aef3aa6b1f4b52bd385be422f811a27c
Mats Mikkor <matsmikkor@pm.me>
2026-02-25 14:13:04 +0200

    Add messages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import { createApp } from 'vue';
 
 
-import { createMemoryHistory ,createWebHashHistory ,createWebHistory ,createRouter } from 'vue-router'
+import { createMemoryHistory, createWebHashHistory, createWebHistory, createRouter } from 'vue-router'
 
 import ToDo from './pages/ToDo.vue';
 import Modals from './pages/Modals.vue';
+import Messages from './pages/Messages.vue';
 
 const routes = [
-    { path: '/', component: ToDo},
-    { path: '/modals', component: Modals },
+    { path: '/', component: ToDo, name: 'Todolist' },
+    { path: '/modals', component: Modals, name: 'Modals' },
+    { path: '/messages', component: Messages, name: 'Messages' }
 ]
 
 export const router = createRouter({
---
+++ src/pages/Messages.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Message from '../components/Message.vue';
+</script>
+
+<template>
+    <Message title="WOW">lol <b>wow</b></Message>
+    <Message color="primary">lol <b>wow</b></Message>
+    <Message color="info">lol <b>wow</b></Message>
+    <Message>lol <b>wow</b></Message>
+</template>
